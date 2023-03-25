ff782ec85c5b8d0144d22e50a9aabe9aaa62da64
DedeMarantes <andre.marantes@gmail.com>
2023-03-25 19:33:45 -0300

    Alteração no main

--- main.go
+++ main.go
@@ -4,16 +4,11 @@ import (
 	"fmt"
 
 	"github.com/DedeMarantes/rest-api-go/database"
-	"github.com/DedeMarantes/rest-api-go/models"
 	"github.com/DedeMarantes/rest-api-go/routes"
 )
 
 func main() {
 	fmt.Println("Iniciando Servidor http://localhost:8500 ")
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "historia1"},
-		{Id: 2, Nome: "nome 2", Historia: "historia2"},
-	}
 	database.ConectaBancoDados()
 	routes.HandleRequest()
 }
